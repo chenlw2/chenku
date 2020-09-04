89bf8af36550930c69c7a5bf5302a389dc6eecdf
chenlw1 <3554922826@qq.com>
2020-09-04 18:22:16 +0800

    2222222

---
+++ src/cn/test/test22.java
@@ -0,0 +1,13 @@
+package cn.test;
+
+public class test22 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("≤‚ ‘222222");
+	}
+
+}
